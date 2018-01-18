2fb0d3a188c974d5079485689ab446276ee14bbd
ПИбд-21, Толкачева Н.С <FireNeko73@yandex.ru>
2018-01-18 13:48:10 +0400

    ??

---
+++ CruiseLiner.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2
+{
+    public class CruiseLiner : Ship
+   
+    {
+        private bool BorderLine;
+        private bool TopLine;
+        private bool RoundLine;
+        private Color dopColor;
+        private bool Rest;
+
+        public CruiseLiner(int maxSpeed,
+                           int maxCountPassenget,
+                           double weight,
+                           Color color,
+                           bool BorderLine,
+                           bool TopLine,
+                           bool RoundLine,
+                           Color dopColor, 
+                           bool Rest,
+                           bool Bass) : base(maxSpeed, maxCountPassenget, weight, color)
+
+        {
+            this.BorderLine = BorderLine;
+            this.TopLine = TopLine;
+            this.RoundLine = RoundLine;
+            this.dopColor = dopColor;
+
+
+        }
+
+        protected override void drawLiner(Graphics g)
+        {
+
+            base.drawLiner(g);
+            Brush dop_color = new SolidBrush(dopColor);
+            Pen pen = new Pen(Color.Black);
+
+            if (BorderLine)
+            {
+                //Боковая линия
+                PointF[] border_line = {
+                    new PointF(startPosX + 5 , startPosY +75),
+                    new PointF(startPosX + 85 , startPosY + 75),
+                    new PointF(startPosX + 80 , startPosY + 90),
+                    new PointF(startPosX + 7 , startPosY + 90)
+                };
+
+                g.FillPolygon(dop_color, border_line);
+                g.DrawPolygon(pen, border_line);
+
+            }
+            if (TopLine)
+            {
+                //Линия на трубе
+                PointF[] top_line = {
+                    new PointF(startPosX + 20 , startPosY + 17),
+                    new PointF(startPosX + 20 , startPosY + 23),
+                    new PointF(startPosX + 30 , startPosY + 23),
+                    new PointF(startPosX + 30 , startPosY + 17)
+                };
+
+                g.FillPolygon(dop_color, top_line);
+                g.DrawPolygon(pen, top_line);
+            }
+            if (RoundLine)
+            {
+                g.FillEllipse(dop_color, startPosX + 20, startPosY + 70, 15, -15);
+                g.DrawEllipse(pen, startPosX + 20, startPosY + 70, 15, -15);
+            }
+
+        }
+        public void setDopColor(Color color)
+        {
+            dopColor = color;
+        }
+
+    }
+}
---
+++ Prichal.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2
+{
+    class Prichal
+    {
+        //код 4 лабораторной
+
+        List<ClassArray<ITransport>> prichalStages;
+        int countPlaces = 20;
+        int placeSizeWidth = 210;
+        int placeSizeHeight = 85;
+        int currentLevel;
+
+        public int getCurrentLevel { get { return currentLevel; } }
+        public Prichal(int countStages)
+        {
+            //как добавить эл-ты в список
+            prichalStages = new List<ClassArray<ITransport>>();
+            ClassArray<ITransport> ship;
+            for (int i = 1; i<= countStages; i++)
+            {
+                ship = new ClassArray<ITransport>(countStages, null);
+                prichalStages.Add(ship);
+            }
+            
+        }
+
+        public void LevelDown()
+        {
+            if (currentLevel + 1 < prichalStages.Count)
+            {
+                currentLevel++;
+            }
+        }
+        public void LevelUp()
+        {
+            if (currentLevel >0)
+            {
+                currentLevel--;
+            }
+        }
+        public int PutShipInPrichal(ITransport ship)
+        {
+            return prichalStages[currentLevel]+ship;
+        }
+        public ITransport GetShipInPrichal(int ticket)
+        {
+            return prichalStages[currentLevel] - ticket;
+        }
+
+        public void Draw(Graphics g)
+        {
+
+            DrawMarking(g);
+
+            for (int i = 0; i < countPlaces; i++)
+            {
+
+                var ship = prichalStages[currentLevel][i];
+                if (ship != null)
+                {
+
+                    ship.setPosition(5 + i / 5 * placeSizeWidth + 5, i % 5 * placeSizeHeight - 15);
+                    ship.drawShip(g);
+                }
+            }
+        }
+
+        private void DrawMarking(Graphics g)
+        {
+
+            Pen pen = new Pen(Color.Black, 3);
+
+            g.DrawString("L" + (currentLevel+1), new Font("Arial",30), new SolidBrush(Color.Blue),(countPlaces / 5)* placeSizeWidth - 70, 420);
+            g.DrawRectangle(pen, 0, 0, (countPlaces / 5) * placeSizeWidth, 480);
+
+            for (int i = 0; i < countPlaces / 5; i++)
+            {
+                for (int j = 0; j < 6; ++j)
+                {
+
+                    g.DrawLine(pen, i * placeSizeWidth, j * placeSizeHeight, i * placeSizeWidth + 110, j * placeSizeHeight);
+                    if (j < 5)
+                {
+                    g.DrawString((i*5+j+1).ToString(), new Font("Arial", 30), new SolidBrush(Color.Blue),i*placeSizeWidth+30,j*placeSizeHeight+20);
+                }
+                }
+               
+                g.DrawLine(pen, i * placeSizeWidth, 0, i * placeSizeWidth, 400);
+            }
+        }
+        
+    }
+}
