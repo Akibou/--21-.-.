7ced2f4c4deb48c72cd378b85989c93e38f39779
ПИбд-21, Толкачева Н.С <FireNeko73@yandex.ru>
2018-01-18 12:49:14 +0400

    lab1

---
+++ RedVine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+
+class RedVine
+{
+    public double Count;
+    private int has_ready = 0;
+    public int Has_ready
+    {
+        get { return has_ready; }
+    }
+    public void GetHeat()
+    {
+        if (has_ready < 10)
+        {
+            has_ready++;
+        }
+    }
+}
